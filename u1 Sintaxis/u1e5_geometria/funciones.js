c619629c06854a6c68ecea44010db0f3d8fc91f3
IESATAULFO\DAW214 <javijuventus@hotmail.com>
2017-10-03 15:57:57 +0200

    no message

---
+++ u1 Sintaxis/u1e5_geometria/funciones.js
@@ -0,0 +1,31 @@
+function  areaTriangulo (b,a){
+return (a*b)/2;
+
+}
+
+function perimetroTrianguloEquilatero(l){
+return l*3;
+}
+
+function perimetroTrianguloIsosceles(l1, l2){
+
+return 2*l1+l2;
+}
+function perimetroTrianguloEscaleno(l1,l2,l3){
+return l1+l2+l3;
+}
+function areaCuadrado(l1){
+return Math.pow(l1,2);
+}
+function perimetroCuadrado(l){
+return 4*l;
+}
+function areaRectangulo(l1,l2){
+return l1*l2;
+}
+function areaCirculo(r){
+return Math.PI*Math.pow(r,2);
+}
+function longitudCircunferencia(r){
+return 2*Math.PI*r;
+}
